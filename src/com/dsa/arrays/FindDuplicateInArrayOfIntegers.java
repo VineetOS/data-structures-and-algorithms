fdd5ecf2a4d42541fffaf8e274b8da5fba1db91d
Vinit Oswal <vinitoswal1697@gmail.com>
2024-07-06 23:34:41 +0530

    find duplicate in an array of N+1 integers

---
+++ src/com/dsa/arrays/FindDuplicateInArrayOfIntegers.java
@@ -0,0 +1,22 @@
+package com.dsa.arrays;
+
+import java.util.Arrays;
+
+public class FindDuplicateInArrayOfIntegers {
+
+    public static int findDuplicate(int[] arr){
+        int n = arr.length, actualSum, expectedSum;
+        expectedSum = (n-1)*((n)/2);
+        actualSum = Arrays.stream(arr).sum();
+        return actualSum - expectedSum;
+    }
+
+    public static void main(String[] args) {
+        /*
+        Find duplicate in an array of N+1 Integers
+         */
+        int[] arr = {1,2,3,4,5,6,9,7};
+        System.out.print(findDuplicate(arr));
+
+    }
+}
