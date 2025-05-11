3cf6a4e346f0e85edf24150216afb5b3beca0217
Vinit Oswal <vinitoswal1697@gmail.com>
2025-05-12 00:07:51 +0530

    copy lonked list with random and next pointer

---
+++ src/com/dsa/linkedlists/NodeRan.java
@@ -0,0 +1,42 @@
+package com.dsa.linkedlists;
+
+public class NodeRan {
+
+    int data;
+    NodeRan next;
+    NodeRan random;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public NodeRan getNext() {
+        return next;
+    }
+
+    public void setNext(NodeRan next) {
+        this.next = next;
+    }
+
+    public NodeRan getRandom() {
+        return random;
+    }
+
+    public void setRandom(NodeRan random) {
+        this.random = random;
+    }
+
+    public NodeRan(int data, NodeRan next, NodeRan random) {
+        this.data = data;
+        this.next = next;
+        this.random = random;
+    }
+
+    public NodeRan(int data) {
+        this.data = data;
+    }
+}
