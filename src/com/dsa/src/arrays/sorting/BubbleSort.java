274577d24a5afb7a8e5f22ac9cd99de864e5a64e
Vinit Oswal <vinitoswal1697@gmail.com>
2025-06-22 01:49:21 +0530

    add sorting algorithms: BubbleSort, InsertionSort, and SelectionSort classes

---
+++ src/com/dsa/src/arrays/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.dsa.src.arrays.sorting;
+
+public class BubbleSort {
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1-i; j++)
+                if(arr[j] > arr[j+1])
+                    swap(arr, j, j+1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
