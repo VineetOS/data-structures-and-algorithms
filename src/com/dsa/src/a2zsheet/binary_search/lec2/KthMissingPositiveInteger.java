c6713c521d629e58ebf293a80348c3c4a70aca97
Vinit Oswal <vinitoswal1697@gmail.com>
2025-07-29 23:28:47 +0530

    add: implement binary search method for finding the kth missing positive integer

--- src/com/dsa/src/a2zsheet/binary_search/lec2/KthMissingPositiveInteger.java
+++ src/com/dsa/src/a2zsheet/binary_search/lec2/KthMissingPositiveInteger.java
@@ -8,11 +8,24 @@ public class KthMissingPositiveInteger {
             else break;
         return k;
     }
-    
+
+    private static int binarySearch(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            int missingCount = arr[mid] - (mid + 1);
+            if(missingCount< k) left = mid + 1;
+            else right = mid - 1;
+        }
+        return  k + right + 1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 3, 4, 7, 11};
         int k = 5;
         int result = bruteForce(arr, k);
         System.out.println("The " + k + "th missing positive integer is: " + result);
+        result = binarySearch(arr, k);
+        System.out.println("The " + k + "th missing positive integer using binary search is" + ": " + result);
     }
 }
