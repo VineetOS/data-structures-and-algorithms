ed8834aade408898fd09cff4ea8d2f7e2c59ae64
Vinit Oswal <vinitoswal1697@gmail.com>
2025-08-17 13:16:47 +0530

    add: implement CombinationSum1 and CombinationSum2 classes for finding unique combinations that sum to a target

---
+++ src/com/dsa/src/dsa_sheet/recursion/KthPermutation.java
@@ -0,0 +1,64 @@
+package com.dsa.src.dsa_sheet.recursion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class KthPermutation {
+
+    static String optimalSolution(int n, int k){
+        int index=0, k1=k-1, temp=1;
+        int[] totalCombos = new int[n];
+        StringBuilder res = new StringBuilder();
+        List<Integer> number = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            temp*=i;
+            totalCombos[i-1] = temp;
+            number.add(i);
+        }
+        while(index<n){
+            int partition = totalCombos[n-index-1]/(n-index);
+            int startIndex = k1/partition;
+            k1 = k1%partition;
+            res.append(number.get(startIndex));
+            number.remove(startIndex);
+            index++;
+        }
+        return res.toString();
+    }
+
+    static String kthPermutation(int n, int k){
+        StringBuilder str = new StringBuilder();
+        ArrayList < String > res = new ArrayList < > ();
+        for (int i = 1; i <= n; i++) {
+            str.append(i);
+        }
+        permHelper(str.toString().toCharArray(),0,res);
+        Collections.sort(res);
+        return res.get(k-1);
+    }
+
+    static void permHelper(char[] str,int index, List<String> perms){
+        if(index == str.length){
+            perms.add(new String(str));
+        }else{
+            for(int i=index; i< str.length; i++){
+                swap(str, i, index);
+                permHelper(str, index+1, perms);
+                swap(str, i, index);
+            }
+        }
+    }
+
+    static void swap(char[] s, int a, int b){
+        char temp = s[a];
+        s[a] = s[b];
+        s[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int n=4, k=17;
+        System.out.println("Brute force: "+ kthPermutation(n,k));
+        System.out.println("Optimal approach: "+ optimalSolution(n,k));
+    }
+}
---
+++ src/com/dsa/src/dsa_sheet/recursion/nqueens/NQueens.java
@@ -0,0 +1,55 @@
+package com.dsa.src.dsa_sheet.recursion.nqueens;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+
+    static List<List<String>> placeNQueens(int n){
+        int[][] board = new int[n][n];
+        List<List<String>> res = new ArrayList<>();
+        placeHelper(board, 0, res);
+        return res;
+    }
+
+    static void placeHelper(int[][] board, int col, List<List<String>> finalRes){
+        if(col == board.length){
+            List<String> currSol = new ArrayList<>();
+            for(int[] row: board){
+                StringBuilder s = new StringBuilder();
+                for(int column: row){
+                    if(column == 1) s.append('Q');
+                    else s.append('.');
+                }
+                currSol.add(s.toString());
+            }
+            finalRes.add(currSol);
+            return;
+        }
+        for(int row = 0; row<board.length; row++){
+            if(canPlaceQueen(board, row, col)){
+                board[row][col] = 1; //place the queen
+                placeHelper(board,col+1,finalRes); //try to place the queen on remaining cols
+                board[row][col] = 0; //backtrack
+            }
+        }
+    }
+
+    static boolean canPlaceQueen(int[][] board, int row, int col){
+
+        for(int j=0; j<col; j++){
+            for(int i=0; i<board.length; i++){
+                //check horizontal direction and vertical direction
+                if(board[i][j] == 1 && (i==row || i-col+j == row || i+col-j == row)) return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        int n=4;
+        List<List<String>> result = placeNQueens(n);
+        System.out.println(result);
+    }
+}
